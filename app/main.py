9ea76d2c1446eb15ac0cf7434b30085dc2f1a846
Jonathan Andres Naranjo <pinto_199728@hotmail.com>
2023-01-19 01:18:33 -0500

    feat: add dockerfile, env configs and middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
+from .config.env_manager import get_settings
+from starlette.middleware.cors import CORSMiddleware
+
+EnvManager = get_settings()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'] if EnvManager.is_dev() else [],
+    allow_origin_regex=EnvManager.ALLOW_ORIGIN_REGEX,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    expose_headers=['X-Total-Count']
+)
 
 handler = Mangum(app)
