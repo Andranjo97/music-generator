18018052caf633b8f78a9a58a75f91b02c608d6d
Jonathan Andres Naranjo <40648805+Andranjo97@users.noreply.github.com>
2023-01-19 01:27:03 -0500

    feat: add dockerfile, env configs and middleware (#1)
    * feat: add dockerfile, env configs and middleware
    
    * fix: fix pipeline
    
    * infra: add on push
    
    * feat: add default route
    
    * infra: test infra

---
+++ app/config/env_manager.py
@@ -0,0 +1,41 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+  ENV: str = 'DEV'
+  OPENAI_API_KEY: str
+  ALLOW_ORIGIN_REGEX: Optional[str]
+
+  class Config:
+    env_file = './.env'
+    env_file_encoding = 'utf-8'
+
+    @classmethod
+    def customise_sources(cls, init_settings, env_settings, file_secret_settings):
+      return (
+        init_settings,
+        env_settings,
+        file_secret_settings,
+      )
+
+  def get(self, key, default=None):
+    return self.dict().get(key, default)
+
+  def is_dev(self):
+    return self.ENV.upper() == 'DEV'
+
+  def is_secure_deployment(self):
+    return self.ENV.upper() in ('STAGE', 'PROD')
+
+
+@lru_cache
+def get_settings() -> Settings:
+  return Settings()
+
+
+@lru_cache
+def get_env_name() -> str:
+  return get_settings().ENV
--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
-app = FastAPI()
+from .config.env_manager import get_settings
+from starlette.middleware.cors import CORSMiddleware
+
+EnvManager = get_settings()
+
+app = FastAPI(title='Music Generator')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'] if EnvManager.is_dev() else [],
+    allow_origin_regex=EnvManager.ALLOW_ORIGIN_REGEX,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    expose_headers=['X-Total-Count']
+)
+
+@app.get("/")
+async def get():
+    return {"app": "Music Generator", "version": "0.0.1", "backend": "lambda"}
 
 handler = Mangum(app)
