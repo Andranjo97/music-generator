9384e772fc65c96a531704641b06be7208a305b2
Jonathan Andres Naranjo <pinto_199728@hotmail.com>
2023-01-19 00:31:51 -0500

    refactor: remove unused code

--- app/main.py
+++ app/main.py
@@ -1,11 +1,3 @@
-# import os
-# import openai
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-# print(openai.Model.list())
-
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
