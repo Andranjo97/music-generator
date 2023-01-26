331ee0ebfbb3b93e18cd24e3ab0f201ff6d9ed4c
Jonathan Andres Naranjo <pinto_199728@hotmail.com>
2023-01-26 00:34:15 -0500

    fix: add correct types

--- app/adapters/services/openai_adapter.py
+++ app/adapters/services/openai_adapter.py
@@ -1,4 +1,5 @@
 import openai
+from typing import Optional
 
 from .base_service_adapter import BaseServiceAdapter
 
@@ -15,7 +16,7 @@ class OpenAIAdapter(BaseServiceAdapter):
     super().__init__()
 
 
-  def get_note_progression(cls, key: Key, scale: Scale, base_key: Key = Key.E) -> NoteProgression:
+  def get_note_progression(cls, key: Key, scale: Scale, base_key: Optional[Key] = Key.E) -> NoteProgression:
     try:
       prompt = cls.generate_ai_prompt(
         progression_type='notes',
@@ -23,8 +24,6 @@ class OpenAIAdapter(BaseServiceAdapter):
         scale=scale, 
         base_key=base_key
       )
-
-      print('???>>>>prompt', prompt)
 
       response = openai.Completion.create(
         model = cls._model,
@@ -49,6 +48,5 @@ class OpenAIAdapter(BaseServiceAdapter):
     pass
 
 
-  def generate_ai_prompt(cls, progression_type: str, key: Key, scale: Scale, base_key: Key):
-    print('IN PROMPT GENEREATION', base_key)
+  def generate_ai_prompt(cls, progression_type: str, key: Key, scale: Scale, base_key: Optional[Key] = Key.E):
     return 'Create a progression of 4 {0}, comma separated, with scientific pitch notation using the {1} scale of {2} with {3} as the base note'.format(progression_type, str(scale), str(key), str(base_key))
